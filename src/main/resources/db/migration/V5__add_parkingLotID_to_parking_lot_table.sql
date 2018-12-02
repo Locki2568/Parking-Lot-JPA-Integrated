087100f2981f3672e897d4e2e521becf65abd32f
Locki Lo <locki2568@gmail.com>
2018-12-02 20:08:05 +0800

    [Locki] fix: modified V5 sql to correctly add unique key to parking_lot

--- src/main/resources/db/migration/V5__add_parkingLotID_to_parking_lot_table.sql
+++ src/main/resources/db/migration/V5__add_parkingLotID_to_parking_lot_table.sql
@@ -1,1 +1,2 @@
-alter table if exists parking_lot add column if not exists parking_lot_id varchar(12) not null
+alter table if exists parking_lot add column if not exists parking_lot_id varchar(12) not null;
+ALTER TABLE parking_lot ADD UNIQUE (parking_lot_id);
